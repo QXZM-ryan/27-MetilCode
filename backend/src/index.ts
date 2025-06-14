a1a1da8f0249c90939eb5e7e5bbcfbc053dd5d23
GabrienConte <gabrielconte0@gmail.com>
2025-06-14 01:39:49 -0300

    Backend e dokcer preparados para desenvolvimento

---
+++ backend/src/index.ts
@@ -0,0 +1,28 @@
+import 'reflect-metadata'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import morgan from 'morgan';
+import { AppDataSource } from './database';
+import { User } from './models/User';
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+// Rota de exemplo
+app.get('/', (req, res) => {
+    res.send('Olá, mundo!');
+});
+
+// Iniciar o servidor
+AppDataSource.initialize().then(() => {
+    app.listen(port, () => {
+        console.log(`Servidor rodando em http://localhost:${port}`);
+    });
+}).catch(error => console.log(error));
